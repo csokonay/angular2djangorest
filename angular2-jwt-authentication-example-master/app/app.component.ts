9e7a9eec5375ec6a57f68fbb7da11a358baf1094
unknown <csokonay.tibor@freemail.hu>
2017-01-19 20:42:59 +0100

    Adding mock-service

--- angular2-jwt-authentication-example-master/app/app.component.ts
+++ angular2-jwt-authentication-example-master/app/app.component.ts
@@ -1,9 +1,26 @@
 ﻿import { Component } from '@angular/core';
+import { Hero } from './mycomponents/hero';
+import { HeroService } from './mycomponents/hero.service';
 
 @Component({
     moduleId: module.id,
     selector: 'app',
-    templateUrl: 'app.component.html'
+    templateUrl: 'app.component.html',
+    providers: [HeroService]
 })
 
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  title = 'Tour of Heroes';
+  heroes: Hero[];
+  selectedHero: Hero;
+  constructor(private heroService: HeroService) { }
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
