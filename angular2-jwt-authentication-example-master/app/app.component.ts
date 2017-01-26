5f44991981a3e18122d78034cd560e85eae6daca
unknown <csokonay.tibor@freemail.hu>
2017-01-26 09:02:40 +0100

    Adding styles

--- angular2-jwt-authentication-example-master/app/app.component.ts
+++ angular2-jwt-authentication-example-master/app/app.component.ts
@@ -8,11 +8,12 @@ import { HeroService } from './mycomponents/hero.service';
   template: `
      <h1>{{title}}</h1>
      <nav>
-        <a routerLink="/dashboard">Dashboard</a>
-        <a routerLink="/heroes">Heroes</a>
+        <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+        <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
      </nav>
      <router-outlet></router-outlet>
-   `
+   `,
+   styleUrls: ['app/app.component.css']
 })
 export class AppComponent {
   heroes: Hero[];
