4fdec54963aa30656b9b01cf62176ca5dcde0fd2
unknown <csokonay.tibor@freemail.hu>
2017-01-26 08:41:36 +0100

    Routing end before styling

--- angular2-jwt-authentication-example-master/app/app.module.ts
+++ angular2-jwt-authentication-example-master/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule }    from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent }  from './app.component';
-import { routing }        from './app.routing';
+import { AppRoutingModule }        from './app.routing';
 
 import { HeroService } from './mycomponents/hero.service';
 import { AuthGuard } from './_guards/index';
@@ -20,7 +20,7 @@ import { DashboardComponent } from './mycomponents/index';
         BrowserModule,
         FormsModule,
         HttpModule,
-        routing
+        AppRoutingModule
     ],
     declarations: [
         AppComponent,
--- angular2-jwt-authentication-example-master/app/app.routing.ts
+++ angular2-jwt-authentication-example-master/app/app.routing.ts
@@ -1,21 +1,31 @@
-﻿import { Routes, RouterModule } from '@angular/router';
+﻿import { NgModule }             from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login/index';
 import { HomeComponent } from './home/index';
 import { AuthGuard } from './_guards/index';
 //import { HeroComponent } from './mycomponents/index';
 import { HeroesComponent, DashboardComponent } from './mycomponents/index';
+import { HeroDetailComponent, EnterMeetingComponent } from './mycomponents/index';
 
-const appRoutes: Routes = [
+const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: '', component: HomeComponent, canActivate: [AuthGuard] },
     //{ path: 'heroapp', component: HeroComponent },
     { path: 'heroes', component: HeroesComponent },
     { path: 'dashboard',component: DashboardComponent },
     { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    { path: 'detail/:id', component: HeroDetailComponent },
+
 
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
 ];
 
-export const routing = RouterModule.forRoot(appRoutes);
+//export const routing = RouterModule.forRoot(appRoutes);
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
